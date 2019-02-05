2455c629b07f78cb88f6824793461e05921f3d0f
Niket Kalia <niketkalia7@gmail.com>
2019-02-05 14:36:16 +0530

    home_widget file commented

--- lib/app_screens/home_widget.dart
+++ lib/app_screens/home_widget.dart
@@ -10,64 +10,64 @@ class Home extends StatefulWidget {
   State createState() => new _HomeState();
 }
 
-class _HomeState extends State<Home> {
-  StreamSubscription<Map<String, double>> _locationSubscription;
-  Location _location = new Location();
-  bool _permission = false;
-  String error;
-  List<Address> addresses;
-  String address="Your Current Address";
+// class _HomeState extends State<Home> {
+//   StreamSubscription<Map<String, double>> _locationSubscription;
+//   Location _location = new Location();
+//   bool _permission = false;
+//   String error;
+//   List<Address> addresses;
+//   String address="Your Current Address";
 
-  Map<String, double> _currentLocation;
-  Map<String, double> _startLocation;
+//   Map<String, double> _currentLocation;
+//   Map<String, double> _startLocation;
 
-  @override
-  void initState() {
-    super.initState();
+//   @override
+//   void initState() {
+//     super.initState();
 
-    initPlatformState();
+//     initPlatformState();
 
-    _locationSubscription =
-        _location.onLocationChanged().listen((Map<String, double> result) {
-      setState(() {
-        _currentLocation = result;
-      });
-    });
-  }
+//     _locationSubscription =
+//         _location.onLocationChanged().listen((Map<String, double> result) {
+//       setState(() {
+//         _currentLocation = result;
+//       });
+//     });
+//   }
 
-  initPlatformState() async {
-    Map<String, double> location;
-    // Platform messages may fail, so we use a try/catch PlatformException.
+//   initPlatformState() async {
+//     Map<String, double> location;
+//     // Platform messages may fail, so we use a try/catch PlatformException.
 
-      try {
-        _permission = await _location.hasPermission();
-        location = await _location.getLocation();
+//       try {
+//         _permission = await _location.hasPermission();
+//         location = await _location.getLocation();
 
-        error = null;
-        final coordinates =
-        new Coordinates(location["latitude"], location["longitude"]);
+//         error = null;
+//         final coordinates =
+//         new Coordinates(location["latitude"], location["longitude"]);
 
-        if (_permission) {
-          addresses =
-          await Geocoder.local.findAddressesFromCoordinates(coordinates);
-          address = addresses.first.addressLine;
-        }
-      } on PlatformException catch (e) {
-        if (e.code == 'PERMISSION_DENIED') {
-          error = 'Permission denied';
-        } else if (e.code == 'PERMISSION_DENIED_NEVER_ASK') {
-          error =
-          'Permission denied - please ask the user to enable it from the app settings';
-        }
+//         if (_permission) {
+//           addresses =
+//           await Geocoder.local.findAddressesFromCoordinates(coordinates);
+//           address = addresses.first.addressLine;
+//         }
+//       } on PlatformException catch (e) {
+//         if (e.code == 'PERMISSION_DENIED') {
+//           error = 'Permission denied';
+//         } else if (e.code == 'PERMISSION_DENIED_NEVER_ASK') {
+//           error =
+//           'Permission denied - please ask the user to enable it from the app settings';
+//         }
 
-        location = null;
-      }
+//         location = null;
+//       }
 
-      setState(() {
-        _startLocation = location;
-      });
+//       setState(() {
+//         _startLocation = location;
+//       });
 
-  }
+//   }
 
   int _currentIndex = 0;
   final List<Widget> _children = [
@@ -80,12 +80,12 @@ class _HomeState extends State<Home> {
     // TODO: implement build
     return Scaffold(
         appBar: AppBar(
-          title: Text(address),
+          //title: Text(address),
 
         ),
         body: _children[_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
-          onTap: onTabTapped,
+          //onTap: onTabTapped,
           currentIndex: _currentIndex,
           items: [
             BottomNavigationBarItem(
@@ -109,9 +109,9 @@ class _HomeState extends State<Home> {
         ));
   }
 
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-}
+//   void onTabTapped(int index) {
+//     //setState(() {
+//       _currentIndex = index;
+//     });
+//   }
+// }
---
+++ lib/app_screens/products.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+
+// class Products extends StatefulWidget {
+//   @override
+//   _ProductsState createState() => _ProductsState();
+// }
+
+// class _ProductsState extends State<Products> {
+//   var product_list = [
+//     {
+//       "name": "Blazer",
+//       "picture": "images/products/blazer1.jpeg",
+//       "old_price": 120,
+//       "price": 85,
+//     },
+//     {
+//       "name": "Red dress",
+//       "picture": "images/products/dress1.jpeg",
+//       "old_price": 100,
+//       "price": 50,
+//     }
+//   ];
